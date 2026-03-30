def9c066b96454bbcaa70d70f6a290031e5fe8cb
Dave Jeffery <dave@davejeffery.com>
2026-03-30 19:14:26 +0100

    build(tray): stop tracking generated dist output

--- apps/tray/dist/app-path.d.ts
+++
@@ -1,5 +0,0 @@
-export interface OpenDialogResult {
-    canceled: boolean;
-    filePaths: string[];
-}
-export declare function chooseCodexAppPath(showOpenDialog: () => Promise<OpenDialogResult>): Promise<string | null>;
